9f2f0807aa30431f4e53daa3aa2116dc72d19de4
Lee <lhy20001030@outlook.com>
2023-04-19 21:28:17 +0800

    Update run.sh

--- roundtriptest/run.sh
+++ roundtriptest/run.sh
@@ -18,23 +18,29 @@ then
     done
 else
     FILES=($BASEDIR/tibby.264 $BASEDIR/black.264 $BASEDIR/../res/BAMQ2_JVC_C.264 $BASEDIR/../res/BA1_FT_C.264 $BASEDIR/walk.264)
-fi
+    # tibby.264等文件为源文件
+fi  # 表示endif
 
-IFS=""
+IFS=""  #Internal Field Seperator，用于确定单词边界
+        #此处表示令输入行所有内容均为字段，避免文件名中的空格引起错误
 
 REPORT=/tmp/report
-rm -f $REPORT
+rm -f $REPORT   #删除/tmp/report
 for f in ${FILES[@]}; do
-    rm -f /tmp/a.pip* /tmp/a.264
-    bill=/tmp/lossless_h264_bill
+    rm -f /tmp/a.pip* /tmp/a.264    #删除所有a.pip开头和a.264开头的文件，包括a.pip本身
+    bill=/tmp/lossless_h264_bill    #存储运行时的信息
+    # tee指令用于将输出复制到文件中并且再在命令行中显示一次， -a用于将cmd内容追加到文件末尾
     echo "============== $f ================" |tee -a $bill
     echo "    ./h264dec $f /tmp/a.pip" | tee -a $bill
+
+    #h264dec 将.264文件 解码为一连串的pip*文件(pip,pip1,...,pip69)
     ./h264dec "$f" /tmp/a.pip | tee -a $bill
-    python analyze_billing.py $bill
-    cat $bill >> $REPORT
-    rm $bill
+    python analyze_billing.py $bill #analyze——billing.py的作用：
+    
+    cat $bill >> $REPORT    #将%bill的内容显示在cmd中，并且存入report
+    rm $bill                #删除bill的内容
     echo "    ./h264dec /tmp/a.pip /tmp/a.264"
     ./h264dec /tmp/a.pip /tmp/a.264
-    diff /tmp/a.264 "$f"
+    diff /tmp/a.264 "$f"    #比较两个文件的区别，并在cmd中显示
 done
 echo "FULL report in $REPORT"
